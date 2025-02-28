a5806389c2e90ebb82839ee95b6fff9ffa75f58f
RubyCucumber79 <kshahnawaz899@gmail.com>
2025-02-28 12:11:08 +0530

    Create main.py

---
+++ todos/main.py
@@ -0,0 +1,23 @@
+todos = []
+
+while True:
+    user_action = input("typye add,show,edit or exit")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for index,item in enumerate(todos):
+                print(index,'-',item)
+        case 'edit':
+            number = input("Number of todos to edit")
+            number = number-1
+            new_todo = input("enter new todo: ")
+            todos[number]=new_todo
+        case 'exit':
+            break
+        
+
+print("Bye!")
