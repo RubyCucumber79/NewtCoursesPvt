02683ba92e83de128f21043e92efe1725b60512a
RubyCucumber79 <kshahnawaz899@gmail.com>
2025-03-03 14:59:07 +0530

    implementAdd

--- todos/gui.py
+++ todos/gui.py
@@ -1,10 +1,22 @@
 import FreeSimpleGUI
+import functions
 
 label = FreeSimpleGUI.Text("Type in a to-do")
-input_box = FreeSimpleGUI.InputText(tooltip="Enter todo")
+input_box = FreeSimpleGUI.InputText(tooltip="Enter todo",key="todo")
 add_button = FreeSimpleGUI.Button("Add")
 window = FreeSimpleGUI.Window("My To-Do App",
                               layout=[[label],[input_box],[add_button]],
                               font=('Helvetica',20))
-window.read()
+while True:
+    event,values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] +"\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case FreeSimpleGUI.WIN_CLOSED:
+            break
 window.close()
