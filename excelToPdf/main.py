6c2f5007ba3641eef6c7ed675e2ef0c715b9c680
RubyCucumber79 <kshahnawaz899@gmail.com>
2025-03-06 11:46:08 +0530

    PackagesUpdate

--- excelToPdf/main.py
+++ excelToPdf/main.py
@@ -1,27 +1,64 @@
-from fpdf import FPDF
 import pandas as pd
-pdf = FPDF(orientation="P",unit="mm",format="A4")
-pdf.set_auto_page_break(auto=False,margin=0)
-df = pd.read_csv("./excelToPdf/topics.csv")
-for index,row in df.iterrows():
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+filepaths = glob.glob("./excelToPdf/invoices/*.xlsx")
+
+for filepath in filepaths:
+
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
-    pdf.set_font(family="Times",style="B",size=24)
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0,h=12,txt=row["Topic"],align="L",ln=1,border=1)
-    pdf.line(10,21,200,21)
 
-    pdf.ln(265)
-    #set footer
-    pdf.set_font(family="Times",style="I",size=8)
-    pdf.set_text_color(180,180,180)
-    pdf.cell(w=0,h=12,txt=row["Topic"],align="R",ln=1,border=1)
-    for i in range(row["Pages"]-1):
-        pdf.add_page()
-         #set footer
-        pdf.ln(265)
-        pdf.set_font(family="Times",style="I",size=8)
-        pdf.set_text_color(180,180,180)
-        pdf.cell(w=0,h=12,txt=row["Topic"],align="R",ln=1,border=1)
+    filename = Path(filepath).stem
+    invoice_nr, date = filename.split("-")
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Add a header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total sum sentence
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Add company name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("./excelToPdf/pythonhow.png", w=10)
 
 
-pdf.output("output.pdf")
+    pdf.output(f"./excelToPdf/PDFs/{filename}.pdf")
